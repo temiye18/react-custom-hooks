b6ab5a10e9c1dec68995b9f3017dc7946e27ba6f
Akinyemi Temiye <akinyemitemiye18@gmail.com>
2022-07-29 14:31:00 +0100

    new commit

---
+++ src/components/NewTask/NewTask.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+import Section from "../UI/Section";
+import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: sendTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    console.log(taskText, taskData);
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = (taskText) => {
+    // const createTask = (taskData) => {
+    //     const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    //     const createdTask = { id: generatedId, text: taskText };
+
+    //     props.onAddTask(createdTask);
+    //   };
+
+    sendTaskRequest(
+      {
+        url: "https://react-http-92e78-default-rtdb.firebaseio.com/tasks.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
